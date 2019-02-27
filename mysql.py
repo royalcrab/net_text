09f935c4703b8e990c40deb711fa8233fea946ac
kaburayalab <sns@kabura-ya.net>
2019-02-27 20:18:21 +0900

    mqtt broker と接続してみるテスト。
    mosquitto が localhost で動いている必要はあり。

--- mysql.py
+++ mysql.py
@@ -5,9 +5,9 @@ args = sys.argv
 pwd = "--password=" + args[1]
 print( pwd )
 
-#p = subprocess.Popen(["mosquitto_sub", "-h localhost", "-t /test"], stdout=subprocess.PIPE)
-#for line in iter(p.stdout.readline,b''):
-#    print(line.rstrip().decode("utf8"))
-#    subprocess.Popen(["mysql", "-u", "kani", "--password=hogehoge", "iot","--execute=show databases"])
+p = subprocess.Popen(["mosquitto_sub", "-h localhost", "-t /test"], stdout=subprocess.PIPE)
+for line in iter(p.stdout.readline,b''):
+    print(line.rstrip().decode("utf8"))
+    subprocess.Popen(["mysql", "-u", "kani", , "iot","--execute=show databases"])
 
-subprocess.Popen(["mysql", "-u", "kani", pwd, "iot","--execute=show databases"])
+# subprocess.Popen(["mysql", "-u", "kani", pwd, "iot","--execute=show databases"])
