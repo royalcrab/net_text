b11296fa71757626411e221465365b3d0087c9ff
kaburayalab <sns@kabura-ya.net>
2019-02-27 19:51:56 +0900

    引数がよめるかのテスト

--- mysql.py
+++ mysql.py
@@ -1,6 +1,9 @@
 import subprocess
 
-p = subprocess.Popen(["mosquitto_sub", "-h localhost", "-t /test"], stdout=subprocess.PIPE)
-for line in iter(p.stdout.readline,b''):
-    print(line.rstrip().decode("utf8"))
-    subprocess.Popen(["mysql", "-u", "kani", "--password=hogehoge", "iot","--execute=show databases"])
+pwd = "--password==%s" % args[1]
+print pwd
+
+#p = subprocess.Popen(["mosquitto_sub", "-h localhost", "-t /test"], stdout=subprocess.PIPE)
+#for line in iter(p.stdout.readline,b''):
+#    print(line.rstrip().decode("utf8"))
+#    subprocess.Popen(["mysql", "-u", "kani", "--password=hogehoge", "iot","--execute=show databases"])
