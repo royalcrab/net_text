8215ed981de0a3af8f0b87655bcc7311ea2ae57d
kaburayalab <sns@kabura-ya.net>
2019-02-27 20:19:10 +0900

    pwd が抜けた (SKKのせい)

--- mysql.py
+++ mysql.py
@@ -8,6 +8,6 @@ print( pwd )
 p = subprocess.Popen(["mosquitto_sub", "-h localhost", "-t /test"], stdout=subprocess.PIPE)
 for line in iter(p.stdout.readline,b''):
     print(line.rstrip().decode("utf8"))
-    subprocess.Popen(["mysql", "-u", "kani", , "iot","--execute=show databases"])
+    subprocess.Popen(["mysql", "-u", "kani", pwd, "iot","--execute=show databases"])
 
 # subprocess.Popen(["mysql", "-u", "kani", pwd, "iot","--execute=show databases"])
