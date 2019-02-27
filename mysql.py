162240935a2f6081cc3d01bd93db2f8b80bead3c
kaburayalab <sns@kabura-ya.net>
2019-02-27 20:20:48 +0900

    なんで全角の句点がはいってる念!

--- mysql.py
+++ mysql.py
@@ -5,7 +5,7 @@ args = sys.argv
 pwd = "--password=" + args[1]
 print( pwd )
 
-p = subprocess.Popen(["mosquitto_sub", "-h"、 "localhost", "-t" 、"/test"], stdout=subprocess.PIPE)
+p = subprocess.Popen(["mosquitto_sub", "-h", "localhost", "-t", "/test"], stdout=subprocess.PIPE)
 for line in iter(p.stdout.readline,b''):
     print(line.rstrip().decode("utf8"))
     subprocess.Popen(["mysql", "-u", "kani", pwd, "iot","--execute=show databases"])
